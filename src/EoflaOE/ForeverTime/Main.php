0c78e2831df802575c86c9647c1b72982376b594
Enrick3344 <enrico3815@hotmail.fr>
2017-09-30 21:07:50 -0400

    Update Main.php

--- src/EoflaOE/ForeverTime/Main.php
+++ src/EoflaOE/ForeverTime/Main.php
@@ -27,7 +27,7 @@ class Main extends PluginBase implements Listener{
 	}
 	
 	public function setTime(){
-		$world = $this->getConfig()->getLevel()->getName();
+		$world = $this->getServer()->getLevelByName();
 		$morning = $this->getConfig()->get("Morning");
 		$night = $this->getConfig()->get("Night");
 		$noon = $this->getConfig()->get("Noon");
